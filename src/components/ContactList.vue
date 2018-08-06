91526f8c64b489abc6df1a4fd7dfe694b9f1998a
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-06 20:36:28 +0200

    TableCSS

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -3,24 +3,24 @@
     <h2>Contact List</h2>
     <form @submit.prevent>
         <label>Name</label>
-        <input v-model="newContact.name" type="text" placeholder="name"><br>
+        <input v-model="newContact.name" type="text" placeholder="name"><br><br> 
         <label>Email</label>
-        <input v-model="newContact.email" type="text" placeholder="email"><br>
-        <button @click="addContact" type="submit">Add Contact</button>
+        <input v-model="newContact.email" type="text" placeholder="email"><br><br>
+        <button @click="addContact" type="submit">Add Contact</button><br><br>
     </form>
-    
-    <table>
-        <thead>
-        <th>Name</th>
-         <th>Email</th>
-        </thead>
-        <tbody>
-            <tr v-for="(contact, key) in contacts" :key="key">
-                <td>{{ contact.name }}</td> 
-                <td>{{ contact.email }}</td> 
-            </tr>
-        </tbody>
-  </table>
+
+  <table id="customers">
+  <tr>
+    <th>Name</th>
+    <th>Email</th>
+  </tr>
+  <tr v-for="(contact, key) in contacts" :key="key"><!--onda umesto ""
+             :key="key"> ide :key="contact.id"> i brise se ovo key u zagradi tj
+             <tr v-for="contact in contacts" :key="contact.id">-->
+   <td>{{ contact.name }}</td> 
+ <td>{{ contact.email }}</td> 
+  </tr>
+</table>
   
   </div> 
 </template>
@@ -33,13 +33,13 @@ export default {
   {
       return { 
       contacts: [
-          {name: "Natasa", email:"test@gmail.com"},
+          {name: "Natasa", email:"test@gmail.com"}, //ako se doda npr id:1 itd onda gore key..
           {name: "Maja", email:"test1@gmail.com"},
           {name: "Marko", email:"test2@gmail.com"},
           {name: "Nikola", email:"test3@gmail.com"},
       ],
 
-      newContact:{
+      newContact:{ // moze ovako a moze i samo prazan objekat kao i dole
           name: '',
           email: ''
       }
@@ -47,8 +47,8 @@ export default {
   },
   methods: {
       addContact(){
-          this.contacts.push(this.newContact);
-          this.newContact = {};
+          this.contacts.push(this.newContact); // pusujemo u listu contacts objekat newContact
+          this.newContact = {};//za resetovanje
       }
   }
 }
@@ -62,5 +62,28 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+#customers {
+    font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+#customers td, #customers th {
+    border: 1px solid #ddd;
+    padding: 8px;
+}
+
+#customers tr:nth-child(even){background-color: #f2f2f2;}
+
+#customers tr:hover {background-color: #ddd;}
+
+#customers th {
+    padding-top: 12px;
+    padding-bottom: 12px;
+    text-align: left;
+    background-color: #4CAF50;
+    color: white;
 }
 </style>
