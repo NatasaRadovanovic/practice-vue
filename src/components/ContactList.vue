2bf45b8938e48d98f6d7d214eda0ab86f6988cf8
NatasaRadovanovic <natasar.radovanovic@gmail.com>
2018-08-06 20:10:07 +0200

    ContactTable

--- src/components/ContactList.vue
+++ src/components/ContactList.vue
@@ -1,14 +1,38 @@
 <template>
-  <div>
-  <h2>Contact List</h2>
-  </div>
-   
+<div>
+    <h2>Contact List</h2>
+    
+    <table>
+        <thead>
+        <th>Name</th>
+         <th>Email</th>
+        </thead>
+        <tbody>
+            <tr v-for="(contact, key) in contacts" :key="key">
+                <td>{{ contact.name }}</td> 
+                <td>{{ contact.email }}</td> 
+            </tr>
+        </tbody>
+  </table>
+  
+  </div> 
 </template>
 
 <script>
 
 export default {
   name: 'ContactList',
+  data()
+  {
+      return { 
+      contacts: [
+          {name: "Natasa", email:"test@gmail.com"},
+          {name: "Maja", email:"test1@gmail.com"},
+          {name: "Marko", email:"test2@gmail.com"},
+          {name: "Nikola", email:"test3@gmail.com"},
+      ]
+    }
+  }
 }
 </script>
 
